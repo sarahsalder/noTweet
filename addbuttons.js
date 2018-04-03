72b39c2535de760d821d152cea5db56063b74b4a
Sarah Alder <sarahsalder@gmail.com>
2018-04-03 18:29:00 -0400

    add some buttons

--- addbuttons.js
+++ addbuttons.js
@@ -1,7 +1,24 @@
-var toolbar = document.getElementsByClassName("TweetBoxToolbar")[0];
-var button = document.createElement("button");
-var text = document.createTextNode("test");
-button.appendChild(text);
-toolbar.appendChild(button);
+let toolbars = document.getElementsByClassName("TweetBoxToolbar");
 
-console.log('hay')
+function addButtons(thing) {
+  let buttons = [
+    {
+      name : document.createTextNode("Text a Friend")
+    },
+    {
+      name : document.createTextNode("Tell Your Therapist Later")
+    },
+    {
+      name : document.createTextNode("Write an Op-Ed")
+    }
+  ]
+  for (let el of buttons) {
+    var button = document.createElement("button");
+    button.appendChild(el.name);
+    thing.appendChild(button);
+  }
+}
+
+for (let toolbar of toolbars) {
+  addButtons(toolbar)
+}
