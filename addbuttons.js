f7b2d2ab38e3009d6665816572a0cbcf035477bf
Sarah Alder <sarahsalder@gmail.com>
2018-04-17 17:29:48 -0400

    updates styling

--- addbuttons.js
+++ addbuttons.js
@@ -1,38 +1,43 @@
 let toolbars = document.getElementsByClassName("TweetBoxToolbar");
+let icons = document.getElementsByClassName("icon-btn");
+let tweetButtons = document.getElementsByClassName("tweet-action");
+
+const ghandiQuotes = [
+  'You must be the change you wish to see in the world. – Mahatma Gandhi',
+  'The weak can never forgive. Forgiveness is the attribute of the strong. – Mahatma Gandhi',
+  'Happiness is when what you think, what you say, and what you do are in harmony. – Mahatma Gandhi',
+  'An eye for eye only ends up making the whole world blind. – Mahatma Gandhi',
+  'Live as if you were to die tomorrow; learn as if you were to live forever. – Mahatma Gandhi',
+  'First they ignore you, then they laugh at you, then they fight you, then you win. – Mahatma Gandhi',
+  'You must not lose faith in humanity. Humanity is an ocean; if a few drops of the ocean are dirty, the ocean does not become dirty. – Mahatma Gandhi',
+  'The best way to find yourself is to lose yourself in the service of others. – Mahatma Gandhi'
+]
 
 function addButtons(toolbar) {
   let buttons = [
     {
-      name : document.createTextNode("Text a Friend"), 
+      type: 'friend',
+      name : document.createTextNode("Tell a Friend"), 
       action: function() {
-        // opens messages, falls back to messenger
+        window.open('https://www.messenger.com','_blank');
+        
       }
     },
     {
-      name : document.createTextNode("Tell Your Therapist"), 
-      action: function() {
-        // opens email (or maybe article on the benefits of having a therapist)
-      }
-    },
-    {
+      type: 'transcendence',
       name : document.createTextNode("Let it Go"), 
       action: function() {
-        const xhr = new XMLHttpRequest();
-        xhr.timeout = 2000;
-        xhr.onreadystatechange = function(e) {
-          if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-            console.log(e)
-            } else {
-              console.log(e)
-            }
-          }
-        }
-        xhr.ontimeout = function () {
-          console.log('timeout')
-        }
-        xhr.open('get', 'https://api.giphy.com/v1/gifs/search?api_key=lvw09FHcNNM92eoAkwgsVM8o761SRb4N?q=cute', true)
-        xhr.send();
+        const randomPosition = Math.floor(Math.random()*ghandiQuotes.length);
+        const content = ghandiQuotes[randomPosition];
+        const gif = '<blockquote class="imgur-embed-pub" lang="en" data-id="a/yF1gV"><a href="//imgur.com/yF1gV"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>';
+        const body = document.getElementsByTagName('body')[0]
+        body.innerHTML = content; 
+        body.style.textAlign = 'center';
+        body.style.top = '40%';
+        body.style.maxWidth = '606px';
+        body.style.margin = '0 auto';
+        body.style.fontSize = '25px';
+        boyd.style.fontStyle = 'italic';
       }
     }
   ]
@@ -40,14 +45,43 @@ function addButtons(toolbar) {
     var button = document.createElement("button");
     button.appendChild(el.name);
     button.classList.add('no-tweet-button')
+    button.classList.add('EdgeButton')
+    button.classList.add('EdgeButton--primary')
+    button.classList.add('js-tweet-btn')
+    if (el.type === 'friend') {
+      button.style.marginLeft = '6px';
+      button.style.marginRight = '6px';
+    }
     button.addEventListener("click", el.action)
     toolbar.appendChild(button);
   }
 }
 
+function removePadding(icon) {
+  icon.style.padding = '2px';
+  icon.style.margin = '2px';
+}
+
 for (let toolbar of toolbars) {
   addButtons(toolbar);
 }
+
+for (let icon of icons) {
+  removePadding(icon);
+}
+
+for (let button of tweetButtons) {
+  button.classList.remove("disabled");
+  button.disabled = false;
+  button.classList.add('no-tweet-button');
+  button.classList.add('EdgeButton');
+  button.classList.add('EdgeButton--primary');
+  button.classList.add('js-tweet-btn');
+}
+
+// const tweetButton = document.getElementsByClassName("TweetBoxToolbar-tweetButton");
+// tweetButton.style.marginLeft = '10px'
+// tweetButton.style.marginRight = '0px'
 
 function addPicToTweet() {
   // url argument can be something like 'https://api.github.com/users/daspinola/repos'
--- background.js
+++ background.js
@@ -3,3 +3,19 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("The color is green.");
   });
 });
+
+chrome.webRequest.onHeadersReceived.addListener(info => {
+  const headers = info.responseHeaders; // original headers
+  for (let i=headers.length-1; i>=0; --i) {
+      let header = headers[i].name.toLowerCase();
+      if (header === "content-security-policy") { // csp header is found
+          // modify frame-src here
+          headers[i].value = headers[i].value.replace("frame-src", "frame-src https://giphycom/");
+      }
+  }
+  // return modified headers
+  return {responseHeaders: headers};
+}, {
+  urls: [ "<all_urls>" ], // match all pages
+  types: [ "sub_frame" ] // for framing only
+}, ["blocking", "responseHeaders"]);
